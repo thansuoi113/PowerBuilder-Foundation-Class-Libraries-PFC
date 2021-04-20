df1c28dc2fa10d9d08c5e36ff496a13dd9a76ecd
PassWords <42901925+thansuoi@users.noreply.github.com>
2021-04-20 09:43:57 +0700

    ADD PB125 Net

---
+++ PB125 Net/Source/demoapp/peat demo app.out/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("peat")]
+[assembly: AssemblyCopyright("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.1")]
